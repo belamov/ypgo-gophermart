ebfba3d06cb424b15d05685ed5f016b178660a3a
belamov <business@belamov.com>
2022-08-04 15:27:28 +0500

    added docker env; added makefile; removed integration tests for now

--- cmd/accrual/main.go
+++ cmd/accrual/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+
 	"github.com/belamov/ypgo-gophermart/internal/gophermart/config"
 )
 
--- internal/gophermart/config/config_test.go
+++ internal/gophermart/config/config_test.go
@@ -9,9 +9,8 @@ import (
 func TestNew(t *testing.T) {
 	t.Run("default config", func(t *testing.T) {
 		c := New()
-		assert.Equal(t, ":8080", c.ServerAddress)
-		assert.Equal(t, "http://localhost:8080", c.BaseURL)
-		assert.Len(t, c.EncryptionKey, 32)
-		assert.NotEmpty(t, c.EncryptionKey)
+		assert.Equal(t, "http://localhost:8080", c.RunAddress)
+		assert.Equal(t, "", c.AccrualSystemAddress)
+		assert.Equal(t, "", c.DatabaseURI)
 	})
 }
