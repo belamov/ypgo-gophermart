7d51798090a5be2a15608c24274d946030e2b8e7
belamov <business@belamov.com>
2022-08-08 00:49:05 +0500

    fixed error

--- internal/gophermart/services/orders_processing_test.go
+++ internal/gophermart/services/orders_processing_test.go
@@ -9,18 +9,18 @@ import (
 func TestOrdersProcessor_ValidateOrderID(t *testing.T) {
 	tests := []struct {
 		name    string
-		orderId int
+		orderID int
 		wantErr bool
 	}{
-		{name: "negative number", orderId: -100, wantErr: true},
-		{name: "0 number", orderId: 0, wantErr: true},
-		{name: "invalid by luhn", orderId: 4561261212345464, wantErr: true},
-		{name: "valid by luhn", orderId: 4561261212345467, wantErr: false},
+		{name: "negative number", orderID: -100, wantErr: true},
+		{name: "0 number", orderID: 0, wantErr: true},
+		{name: "invalid by luhn", orderID: 4561261212345464, wantErr: true},
+		{name: "valid by luhn", orderID: 4561261212345467, wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			ordersProcessor := NewOrdersProcessor()
-			err := ordersProcessor.ValidateOrderID(tt.orderId)
+			err := ordersProcessor.ValidateOrderID(tt.orderID)
 			if tt.wantErr {
 				assert.Error(t, err)
 			} else {
