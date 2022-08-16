1f4e7fc5c0fd66a6a56329c38d95378d8d1022d0
belamov <business@belamov.com>
2022-08-17 02:25:12 +0500

    fixed order id type

--- internal/accrual/storage/migrations/000001_create_orders_table.up.sql
+++ internal/accrual/storage/migrations/000001_create_orders_table.up.sql
@@ -1,5 +1,5 @@
 create table if not exists orders(
-    id int unique not null,
+    id bigint unique not null,
     created_at timestamp not null default now(),
     status smallint not null,
     accrual decimal
--- internal/accrual/storage/migrations/000002_create_order_items_table.up.sql
+++ internal/accrual/storage/migrations/000002_create_order_items_table.up.sql
@@ -1,5 +1,5 @@
 create table if not exists order_items(
-    order_id int not null,
+    order_id bigint not null,
     description text not null,
     price decimal not null,
     constraint fk_order_id foreign key(order_id) references orders(id)
